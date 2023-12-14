5d9cfe8339e67e42dbac3e4d3c294569e0f1d688
Veasna <thamveasna123@gmail.com>
2023-12-14 10:36:49 +0700

    1.3

--- src/Books/Books.js
+++ src/Books/Books.js
@@ -277,19 +277,6 @@ function Books(){
                         </section>
                     </div>
                 </section>
-                <section className='card'>
-                    <img src="https://cdn.kobo.com/book-images/ef8112fe-15c4-4d30-bc4e-91914c30afa7/1200/1200/False/solo-leveling-vol-2-comic.jpg" 
-                        alt="Solo Leveling Vol. 2" 
-                        className='card-img'/>
-                    <div className="card-details">
-                        <h3 className="class-title">Solo Leveling Vol. 2</h3>
-                        <section className="card-price">
-                        <div className="price">
-                            $19.99
-                        </div>
-                        </section>
-                    </div>
-                </section>
             </section>
         </>
     );
